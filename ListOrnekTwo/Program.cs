54811b786b2534ea08e0f6d984150aa07a737946
Ceydaay <cydylmz.99@gmail.com>
2024-08-25 01:20:16 +0300

    Add project files.

---
+++ ListOrnekTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        List<string> kahveListesi = new List<string>();
+
+        Console.WriteLine("Lütfen 5 adet kahve ismi giriniz:");
+
+        for (int i = 0; i < 5; i++) {
+
+            Console.Write("Kahveniz" +  (i+1) + ":");
+            string kahveIsmi = Console.ReadLine(); 
+            kahveListesi.Add(kahveIsmi);
+        
+        }
+
+        Console.WriteLine("Girdiğiniz Kahveler:");
+        foreach (string kahve in kahveListesi) { Console.WriteLine(kahve); }
+    }
+}
